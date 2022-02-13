b4f09972b3f85478231dc1b5b51b34a248439240
Steffen Nobis <steffen@webnobis.com>
2022-02-13 17:19:31 +0100

    current needed aggregation added and logic changed to adapt deprecated method

--- src/main/java/com/webnobis/consumption/business/impl/ConsumptionServiceImpl.java
+++ src/main/java/com/webnobis/consumption/business/impl/ConsumptionServiceImpl.java
@@ -2,24 +2,21 @@ package com.webnobis.consumption.business.impl;
 
 import java.time.Month;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.webnobis.consumption.business.ConsumptionService;
 import com.webnobis.consumption.model.Consumption;
+import com.webnobis.consumption.model.Coverage;
 import com.webnobis.consumption.model.Medium;
 import com.webnobis.consumption.repository.RepositoryService;
 
 public class ConsumptionServiceImpl implements ConsumptionService {
-
-	private static final Logger log = LoggerFactory.getLogger(ConsumptionServiceImpl.class);
 
 	private final RepositoryService repositoryService;
 
@@ -29,68 +26,46 @@ public class ConsumptionServiceImpl implements ConsumptionService {
 
 	@Override
 	public SortedSet<Consumption> getConsumptions(Medium medium, Collection<Integer> years, boolean monthly) {
-		Objects.requireNonNull(medium, "medium is null");
-		Objects.requireNonNull(years, "years is null");
-		
-		final SortedSet<Consumption> consumptions = getConsumptions(medium, years);
 		if (monthly) {
-			log.info("found " + consumptions.size() + " monthly consumptions");
-			return consumptions;
+			return new TreeSet<>(getMonthlyAnnualConsumptions(medium, years));
 		} else {
-			SortedSet<Integer> yearsWithYearBefore =new TreeSet<>(years);
-			yearsWithYearBefore.add(yearsWithYearBefore.first() - 1);
-			SortedSet<Consumption> sumConsumptions = Stream.concat(consumptions.stream()
-					.map(Consumption::getYear)
-					.map(year -> toYearSumConsumption(consumptions, year)), Stream.of(toLast12MonthSumConsumption(getConsumptions(medium, yearsWithYearBefore))))
-					.distinct()
-					.collect(Collectors.toCollection(TreeSet::new));
-			log.info("found " + sumConsumptions.size() + " yearly consumptions");
-			return sumConsumptions;
+			return new TreeSet<>(getAnnualConsumptions(medium, years, false));
 		}
-	}
-	
-	private SortedSet<Consumption> getConsumptions(Medium medium, Collection<Integer> years) {
-		if (years.isEmpty()) {
-			return Collections.emptySortedSet();
-		}
-		int lastYear = new TreeSet<>(years).last();
-		return repositoryService.findCoverages()
-				.stream()
-				.filter(coverage -> medium.equals(coverage.getMedium()))
-				.filter(coverage -> coverage.getYear() <= lastYear)
-				.sorted()
-				.map(new CoverageToConsumptionTransformer())
-				.filter(consumption -> years.contains(consumption.getYear()))
-				.collect(Collectors.toCollection(TreeSet::new));
-	}
-	
-	private Consumption toYearSumConsumption(SortedSet<Consumption> yearConsumptions, int year) {
-		Consumption last = yearConsumptions.last();
-		float yearSum = (float) yearConsumptions.parallelStream()
-			.filter(consumption -> year == consumption.getYear())
-			.mapToDouble(consumption -> consumption.getConsumption())
-			.sum();
-		return new Consumption(year, Month.DECEMBER, last.getMedium(), last.getDialCount(), yearSum, false);
-	}
-	
-	private Consumption toLast12MonthSumConsumption(SortedSet<Consumption> yearConsumptions) {
-		Consumption last = yearConsumptions.last();
-		float last12MonthSum = (float) yearConsumptions.parallelStream()
-			.filter(consumption -> withinLast12MonthRange(consumption, last.getMonth(), last.getYear()))
-			.peek(System.out::println)
-			.mapToDouble(consumption -> consumption.getConsumption())
-			.sum();
-		return new Consumption(last.getYear(), last.getMonth(), last.getMedium(), last.getDialCount(), last12MonthSum, false);
 	}
 
-	private boolean withinLast12MonthRange(Consumption checkConsumption, Month lastMonth, int lastYear) {
-		if (checkConsumption.getYear() == lastYear) {
-			return true;
-		}
-		if (checkConsumption.getYear() == (lastYear - 1)) {
-			return checkConsumption.getMonth().compareTo(lastMonth) > 0;
-		}
-		return false;
+	@Override
+	public List<Consumption> getMonthlyAnnualConsumptions(Medium medium, Collection<Integer> years) {
+		Objects.requireNonNull(medium);
+		Objects.requireNonNull(years);
+		Function<Coverage, Consumption> transformer = new CoverageToConsumptionTransformer();
+		return repositoryService.findCoverages().parallelStream()
+				.filter(consumption -> medium.equals(consumption.getMedium()))
+				.filter(consumption -> years.contains(consumption.getYear())).map(transformer).sorted().toList();
 	}
-	
+
+	@Override
+	public List<Consumption> getAnnualConsumptions(Medium medium, Collection<Integer> years, boolean andLast12Month) {
+		return Stream.concat(getAnnualConsumptions(medium, years),
+				andLast12Month ? getLast12MonthConsumptions(medium, years) : Stream.empty()).toList();
+	}
+
+	private Stream<Consumption> getAnnualConsumptions(Medium medium, Collection<Integer> years) {
+		return getMonthlyAnnualConsumptions(medium, years).stream()
+				.collect(Collectors.groupingBy(consumption -> consumption.getYear())).entrySet().stream()
+				.map(entry -> toSum(entry.getValue(), medium, entry.getKey()));
+	}
+
+	private Stream<Consumption> getLast12MonthConsumptions(Medium medium, Collection<Integer> years) {
+		List<Consumption> tmpList = getMonthlyAnnualConsumptions(medium, years);
+		return Stream.of(toSum(tmpList.subList(Math.max(0, tmpList.size() - 12), tmpList.size()), medium,
+				new TreeSet<>(years).last()));
+	}
+
+	private static Consumption toSum(Collection<Consumption> consumptions, Medium missingMedium, int missingYear) {
+		return consumptions.parallelStream()
+				.reduce((c1, c2) -> new Consumption(c1.getYear(), Month.DECEMBER, c1.getMedium(),
+						Math.max(c1.getDialCount(), c2.getDialCount()), c1.getConsumption() + c2.getConsumption(),
+						false))
+				.orElse(new Consumption(missingYear, Month.DECEMBER, missingMedium, 0, 0, false));
+	}
 }

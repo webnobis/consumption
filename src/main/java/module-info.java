c336b0d2e1054284cefc822bd9eb426eb7d18806
Steffen Nobis <steffen@webnobis.com>
2022-02-15 20:48:09 +0100

    model also exported

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.webnobis.consumption {
 	requires org.jfree.jfreechart;
 
 	exports com.webnobis.consumption;
+	exports com.webnobis.consumption.model;
 	exports com.webnobis.consumption.repository.file.migrator.v1;
 
 }
